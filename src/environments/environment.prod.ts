20c3383066642945f26f9231f239dbea2011412a
wilyramos <wily.ramos1@unmsm.edu.pe>
2025-10-28 14:23:51 -0500

    add environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
-// File: frontend/src/environments/environment.ts
-
+// File: frontend/src/environments/environment.prod.ts
 export const environment = {
   production: true,
   apiUrl: 'https://ecommerce-nestjs-mongodb-backend.onrender.com',
